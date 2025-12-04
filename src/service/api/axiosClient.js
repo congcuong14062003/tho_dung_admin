59d17f166cab5cbb6e9722dfee6e10121736038d
DESKTOP-6APP6KP\APC <cuongkoi1406@gmail.com>
2025-12-04 16:53:47 +0700

    update

--- src/service/api/axiosClient.js
+++ src/service/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosClient = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/apis`,  
+  baseURL: `https://tho-dung-server.onrender.com/apis`,  
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
