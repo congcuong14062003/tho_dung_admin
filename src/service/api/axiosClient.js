e6a4be8965d4638785c18dc0eb56b420e7086588
DESKTOP-6APP6KP\APC <cuongkoi1406@gmail.com>
2025-12-04 16:15:25 +0700

    update

--- src/service/api/axiosClient.js
+++ src/service/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:2003/apis",
+  baseURL: import.meta.env.VITE_API_URL,  
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
